2e7953183cebf815f7e0a753e5fc67ea868acba9
Vamsidhar Reddy Menthem <85726151+vamsidhar18@users.noreply.github.com>
2022-12-03 13:20:57 -0800

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import AllRoutes from './AllRoutes';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <AllRoutes></AllRoutes>
+            </Router>
+            <ToastContainer
+                position="top-center"
+                autoClose={2500}
+                theme="dark"
+            />
+        </div>
+    );
 }
 
 export default App;
